535073eaf915eb3fdb34aaa2c1a67aba6f398d7f
adlerjani <adlerphotobusiness@gmail.com>
2022-03-07 11:59:50 +0100

    komment

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -81,6 +81,7 @@ class Game extends React.Component {
   render() {
     const history = this.state.history;
     const current = history[history.length - 1];
+    //Itt kéne egy ilyet csinálni a dokumentáció szerint de nem okés: const current = history[this.state.stepNumber];
     const winner = calculateWinner(current.squares);
 
     const moves = history.map((step, move) => {
