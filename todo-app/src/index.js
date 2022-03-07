40598e98854f9c3c7e7173f279f3670bd39c0283
adlerjani <adlerphotobusiness@gmail.com>
2022-03-07 22:50:34 +0100

    MUI telepítés és gyakorlás

--- todo-app/src/index.js
+++ todo-app/src/index.js
@@ -1,13 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import Button from '@mui/material/Button';
+//import Card from '@mui/material/Card';
+
+import BasicCard from './card.js';
+
 import './index.css';
 
 
 function App(){
     return(
+        
         <body>
             <h1 class="header-text">To-Do App</h1>
+            <Row>
+            <BasicCard />
+            </Row>
+            
+            
+            
+            
         </body>
         
     )
